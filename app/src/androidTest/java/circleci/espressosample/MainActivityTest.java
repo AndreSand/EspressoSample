92e7cf4717f335d2e9c96b89c238abf4a721950e
Andres Sandoval <andres.sandoval@teamaol.com>
2016-03-10 14:34:39 -0800

    Adding Spoon reporting. Currently running Espresso UI test and reports with using Spoon display nice results.

--- app/src/androidTest/java/circleci/espressosample/MainActivityTest.java
+++ app/src/androidTest/java/circleci/espressosample/MainActivityTest.java
@@ -4,9 +4,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+
+import com.squareup.spoon.Spoon;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -20,13 +24,21 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
+
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(
             MainActivity.class);
 
     @Test
     public void testTextChangesWhenButtonIsPressed() {
+        Spoon.screenshot(mainActivityActivityTestRule.getActivity(), "start_state");
         onView(withId(R.id.changeTextButton)).perform(click());
         onView(withId(R.id.helloWorldTextView)).check(matches(withText("Text changed!")));
+        Spoon.screenshot(mainActivityActivityTestRule.getActivity(), "end_state");
+    }
+
+    @Test
+    public void testTextTakePic() {
+        Spoon.screenshot(mainActivityActivityTestRule.getActivity(), "initial_state");
     }
 }
